7a197bf8c0deca9ace4e044fa7e6483f722923f3
kyle.a.lewis <kyle.a.lewis@accenture.com>
2025-09-15 15:11:45 +0100

    Updated Test

--- Test.java
+++ Test.java
@@ -1,7 +1,11 @@
-public class Test{
-    public static void main(String[] args){
+public class Test {
+    public static void main(String[] args) {
 
+        System.out.println("Hello and welcome");
 
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Wow");
+            }
+            System.out.println("Woo, I did a thing");
     }
-
 }
